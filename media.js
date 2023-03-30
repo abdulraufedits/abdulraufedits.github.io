5a88ffd6c763910d86a88f6d3c311b16d3f01e48
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-03-30 23:22:30 +0500

    major update to img optimization

---
+++ media.js
@@ -0,0 +1,15 @@
+let proj1 = document.getElementById("project--1");
+let proj2 = document.getElementById("project--2");
+let proj3 = document.getElementById("project--3");
+let proj4 = document.getElementById("project--4");
+let proj5 = document.getElementById("project--5");
+
+proj1.style.setProperty('background-image', 'url(https://lh3.google.com/pw/AMWts8A7muaVTikMLjRbGGRN2X8JgZWD1wYNUVcdgUc0CpuPCOE3Dg6xSyGXkilvvBpB08nTaAaI1V99gJfEAtONOcWf024iqTE=w1198-h799-s-no?authuser=0)');
+proj2.style.setProperty('background-image', 'url(https://lh3.google.com/pw/AMWts8CoY9C93VQQMt2huu9ss1EutJ1UJ11MkhIug9O1KeBfplblRi9IBsVdDagQ2Ky4QtfE_Q_0laB3_al5hCXm2fVQCFq72GE=w1124-h799-s-no?authuser=0)');
+proj3.style.setProperty('background-image', 'url(https://lh3.google.com/pw/AMWts8B03u6mz-LbqY9tSY_i0d8zgCpjKDEtbYiKIxuZnff2Dm6NIyfH3VhTtQFvanGcCt1LJ3qRqtU0ohLuYb-_LyFQFTVy0Dk=w1420-h799-s-no?authuser=0)');
+proj4.style.setProperty('background-image', 'url(https://lh3.google.com/pw/AMWts8DXYFUaaNrYhIZUkBM-EVkiXS4hWMEd4gZH_93bxzQ2AM01NoIZmO9YEHaNH1zpVpgpBpauugDZHyJwEHppkGhlNce4wvw=w1124-h799-s-no?authuser=0)');
+proj5.style.setProperty('background-image', 'url(https://lh3.google.com/pw/AMWts8AYv-SeomGkGO95T5vHIdVn-sw9S2LoTxqhYKENwupU8ycGY2pK7BW9GFLynt9Eu4mDpyQNJORhoa35iPDtswGy85F8=w1043-h799-s-no?authuser=0)');
+
+let abtImg = document.getElementById('abt--img');
+
+abtImg.setAttribute("src", "https://lh3.google.com/pw/AMWts8ATWcHS_gtlelKgGS08ZaLgBvnCQC_XTmsOfoTrTuAcAXwsDtS-eD_L0U14KCYGFaMllHvV6DF8vv9R7LffNTe5o6ERBg=w799-h799-s-no?authuser=0");
