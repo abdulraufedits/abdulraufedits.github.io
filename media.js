eee115f1f9d8fbdbcafd504b8ef681372b691a9a
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-03-31 17:00:23 +0500

    final update to imgs

--- media.js
+++ media.js
@@ -5,11 +5,11 @@ let proj4 = document.getElementById("project--4");
 let proj5 = document.getElementById("project--5");
 
 proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/iPhone_X_-__light.jpg)');
-proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/https://ik.imagekit.io/abduledits5/thumbnail.png)');
+proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
 proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades1__4k_.png)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
 proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb_penpot.png)');
 
 let abtImg = document.getElementById('abt--img');
 
-abtImg.setAttribute("src", "https://lh3.google.com/pw/AMWts8ATWcHS_gtlelKgGS08ZaLgBvnCQC_XTmsOfoTrTuAcAXwsDtS-eD_L0U14KCYGFaMllHvV6DF8vv9R7LffNTe5o6ERBg=w799-h799-s-no?authuser=0");
+abtImg.setAttribute("src","https://ik.imagekit.io/abduledits5/new_avi.jpg");
