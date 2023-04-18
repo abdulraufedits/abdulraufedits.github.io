3432e85be7a6a38a1dc58c34e54ce2127f1421ec
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-04-18 13:27:56 +0500

    android proj addition

--- media.js
+++ media.js
@@ -1,14 +1,17 @@
-let proj1 = document.getElementById("project--1");
-let proj2 = document.getElementById("project--2");
-let proj3 = document.getElementById("project--3");
-let proj4 = document.getElementById("project--4");
 let proj5 = document.getElementById("project--5");
+let proj4 = document.getElementById("project--4");
+let proj3 = document.getElementById("project--3");
+let proj2 = document.getElementById("project--2");
+let proj1 = document.getElementById("project--1");
+
+let proj6 = document.getElementById("project--6");
 
 proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/iPhone_X_-__light.jpg)');
 proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
 proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades1__4k_.png)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
 proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb_penpot.png)');
+proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb.png)');
 
 let abtImg = document.getElementById('abt--img');
 
