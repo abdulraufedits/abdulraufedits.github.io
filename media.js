4b6f4896ea99eadaa13be8aa5cacb98298f7d631
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-04-19 20:29:27 +0500

    a major change in projects and expertise keys

--- media.js
+++ media.js
@@ -5,13 +5,15 @@ let proj2 = document.getElementById("project--2");
 let proj1 = document.getElementById("project--1");
 
 let proj6 = document.getElementById("project--6");
+let proj7 = document.getElementById("project--7");
 
-proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/iPhone_X_-__light.jpg)');
+proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/melodica_thumb.png)');
 proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
 proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades1__4k_.png)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
 proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb_penpot.png)');
-proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb.png)');
+proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/framer_thumb.png)');
+proj7.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb.png)');
 
 let abtImg = document.getElementById('abt--img');
 
