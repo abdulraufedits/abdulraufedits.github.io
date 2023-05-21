8053e72629cead58407431be470613c764f74827
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-05-21 11:44:51 +0500

    update to project padding

--- media.js
+++ media.js
@@ -9,7 +9,7 @@ let proj7 = document.getElementById("project--7");
 
 proj1.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/melodica_thumb.png)');
 proj2.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumbnail.png)');
-proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades1__4k_.png)');
+proj3.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/ades_thumb.png)');
 proj4.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/am_1.png)');
 proj5.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/thumb_penpot.png)');
 proj6.style.setProperty('background-image', 'url(https://ik.imagekit.io/abduledits5/framer_thumb.png)');
