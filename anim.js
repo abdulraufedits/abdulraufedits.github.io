899ffa5071c1e80aba03dd5b67f69ef2cf657c0e
Abdul Rauf <editsabdulrauf5681@gmail.com>
2023-07-22 22:45:48 +0500

    removed unnecessary anim code

--- anim.js
+++ anim.js
@@ -1,24 +1,5 @@
 const textAnim = new SplitType(".text_anim", { types: 'lines,  words'});
 
-function gsapAnimation(elm){
-        elm.forEach(el => {
-          gsap.to(el,{
-            // yes, we can add it to an entire timeline!
-            y: 0 ,
-            scrollTrigger: {
-              Trigger: el,
-              start: "top top",
-              markers: true
-            },
-            stagger: 0.045,
-            delay: 0.2,
-            duration: 0.6,
-            ease: "circ.inOut"
-          });
-  
-        });
-       
-}
 
 // gsapAnimation(document.querySelectorAll('.name .line .word'));
 // gsapAnimation(document.querySelectorAll('.abt-p .line .word'));
