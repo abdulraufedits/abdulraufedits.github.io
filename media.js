8d9eff7fb22f2e2f02f79331850d867f3874616d
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-01-11 09:05:04 +0500

    major update to my work section

--- media.js
+++ media.js
@@ -6,7 +6,7 @@ resumeCtas.forEach(resumeCta => {
 
 let projImgs = document.querySelectorAll('.proj-img');
 
-let projectImgs = ["https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png", "https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png", "https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png", "https://ik.imagekit.io/abduledits5/am_1.png"]
+let projectImgs = ["https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png", "https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png", "https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png", "https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png", "https://ik.imagekit.io/abduledits5/am_1.png", "https://ik.imagekit.io/abduledits5/thumbnail.png"]
 
 for (let i = 0; i < projectImgs.length; i++) {
     projImgs[i].style.backgroundImage = `url(${projectImgs[i]})`;
