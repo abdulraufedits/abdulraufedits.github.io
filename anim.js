aa1e173daa07df3c394d97e91b8e038cc3c801c1
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-01-11 11:19:33 +0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -7,8 +7,9 @@ const textAnim = new SplitType(".text_anim", { types: 'lines,  words'});
 gsap.to(document.querySelectorAll('.name .line .word'),{
   // yes, we can add it to an entire timeline!
   y: 0 ,
-  stagger: 0.039,
-  delay: 0.2,
-  duration: 0.4,
-  ease: "circ.inOut"
+  stagger: 0.05,
+  delay: 0.5,
+  duration: 0.1,
+  ease: "power4.out"
 });
+
