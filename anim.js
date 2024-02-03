c50e277aa4c207d1e3be2bedb9f085563cddb79a
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-02-03 09:16:51 +0500

    gsap animation

--- anim.js
+++ anim.js
@@ -1,15 +1,47 @@
-const textAnim = new SplitType(".text_anim", { types: 'lines,  words'});
+const textAnim1 = new SplitType(".text_anim", { types: 'lines,  words'});
+const textAnim2 = new SplitType(".p-text-anim", { types: 'lines words'});
 
 
 // gsapAnimation(document.querySelectorAll('.name .line .word'));
 // gsapAnimation(document.querySelectorAll('.abt-p .line .word'));
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(document.querySelectorAll('.name .line .word'),{
+gsap.to('.name .line .word',{
   // yes, we can add it to an entire timeline!
   y: 0 ,
-  stagger: 0.05,
+  stagger: 0.01,
   delay: 0.5,
-  duration: 0.1,
-  ease: "power4.out"
+  duration: 0.3    ,
+  ease: "sine.out"
 });
+gsap.to('.p-text-anim .line .word',{
+  y: 0 ,
+  stagger: 0.01,
+  delay: 1,
+  duration: 0.3,
+  ease: "sine.out"
+});
+gsap.to(".ctas", {
+  y: 0,
+  opacity: 1,
+  delay: 1.2,
+  duration: 0.5,
+  ease: "sine.out"
+})
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".proj-h1",
+    start: "top 80%",
+    end: "bottom center",
+    scrub: true
+  }
+})
 
+tl.to(".proj-h1", {
+  opacity: 1,
+  y: 0
+})
+tl.to(".project-sec", {
+  opacity: 1,
+  y: 0
+})
