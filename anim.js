117a246236ec4ccf31291a1c0adb3160b67c7c67
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-02-04 19:30:24 +0500

    animation update

--- anim.js
+++ anim.js
@@ -2,23 +2,13 @@ const textAnim1 = new SplitType(".text_anim", { types: 'lines,  words'});
 const textAnim2 = new SplitType(".p-text-anim", { types: 'lines words'});
 
 
-// gsapAnimation(document.querySelectorAll('.name .line .word'));
-// gsapAnimation(document.querySelectorAll('.abt-p .line .word'));
-gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.name .line .word',{
+gsap.to('.word',{
   // yes, we can add it to an entire timeline!
   y: 0 ,
-  stagger: 0.01,
+  stagger: 0.05,
   delay: 0.5,
-  duration: 0.3    ,
-  ease: "sine.out"
-});
-gsap.to('.p-text-anim .line .word',{
-  y: 0 ,
-  stagger: 0.01,
-  delay: 1,
-  duration: 0.3,
+  duration: 0.5,
   ease: "sine.out"
 });
 gsap.to(".ctas", {
