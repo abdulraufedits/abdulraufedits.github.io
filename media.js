25dd68d67cf53c06a54fa3ba36ac63d3de4c107b
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-11-29 10:59:57 +0000

    finnote image updated

--- media.js
+++ media.js
@@ -6,7 +6,7 @@ resumeCtas.forEach(resumeCta => {
 
 let projImgs = document.querySelectorAll('.proj-img');
 
-let projectImgs = ["https://ik.imagekit.io/abduledits5/pfolioMe/ascend_thumb.png?updatedAt=1716746945751","https://ik.imagekit.io/abduledits5/pfolioMe/ascend_thumb.png?updatedAt=1716746945751","https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png?updatedAt=1716738142375", "https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png?updatedAt=1716735500016", "https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png?updatedAt=1716736659216", "https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png?updatedAt=1716735465871"]
+let projectImgs = ["https://ik.imagekit.io/abduledits5/pfolioMe/finnote_thumb.png","https://ik.imagekit.io/abduledits5/pfolioMe/ascend_thumb.png?updatedAt=1716746945751","https://ik.imagekit.io/abduledits5/pfolioMe/mathtrove_thumb.png?updatedAt=1716738142375", "https://ik.imagekit.io/abduledits5/pfolioMe/andr15_thumb.png?updatedAt=1716735500016", "https://ik.imagekit.io/abduledits5/pfolioMe/framer_thumb.png?updatedAt=1716736659216", "https://ik.imagekit.io/abduledits5/pfolioMe/penpot_thumb.png?updatedAt=1716735465871"]
 let projColors = ["#0052CC","#2a2ea7","#111111", "#006465", "#003f65","#005d4a"];
 for (let i = 0; i < projectImgs.length; i++) {
     projImgs[i].style.backgroundImage = `url(${projectImgs[i]})`;
