3c155f18077c8057014357231bb492eec1220df2
Abdul Rauf <editsabdulrauf5681@gmail.com>
2025-02-03 18:41:59 -0800

    contact .html removed

--- media.js
+++ media.js
@@ -1,7 +1,7 @@
 var resumeCtas = document.querySelectorAll('.resume-cta');
 
 resumeCtas.forEach(resumeCta => {
-    resumeCta.setAttribute('href', 'https://drive.google.com/file/d/1-Fy5xB9dxoB59Ywi9nNgrU5azkkjdnRm/view');
+    resumeCta.setAttribute('href', 'https://drive.google.com/file/d/1S6Yf2S21PcCrBYkouA_IK4tF7YSVbw3I/view');
 });
 
 let projImgs = document.querySelectorAll('.proj-img');
